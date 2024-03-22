2dacabc3bfbc31fe139c77b14602101bb93025ac
MrYsLab <MisterYsLab@gmail.com>
2024-03-22 17:11:11 -0400

    Esp32 support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('pypi_desc.md') as f:
 
 setup(
     name='s3-extend',
-    version='1.21',
+    version='1.30',
     packages=[
       's3_extend',
       's3_extend.gateways'
@@ -16,7 +16,8 @@ setup(
         'pymata_rh',
         'pymata-cpx',
         'tmx-pico-aio',
-        'telemetrix-aio>=1.8'
+        'telemetrix-aio>=1.8',
+        'telemetrix-esp32'
     ],
 
     entry_points={
@@ -24,6 +25,7 @@ setup(
             's3a = s3_extend.s3a:s3ax',
             's3c = s3_extend.s3c:s3cx',
             's3e = s3_extend.s3e:s3ex',
+            's32 = s3_extend.s32:s32ex',
             's3p = s3_extend.s3p:s3px',
             's3r = s3_extend.s3r:s3rx',
             's3rh = s3_extend.s3rh:s3rhx',
@@ -31,6 +33,7 @@ setup(
             'ardgw = s3_extend.gateways.arduino_gateway:arduino_gateway',
             'cpxgw = s3_extend.gateways.cpx_gateway:cpx_gateway',
             'espgw = s3_extend.gateways.esp8266_gateway:esp8266_gateway',
+            'esp32gw = s3_extend.gateways.esp32_gateway:esp32_gateway',
             'pbgw = s3_extend.gateways.picoboard_gateway:picoboard_gateway',
             'rpigw = s3_extend.gateways.rpi_gateway:rpi_gateway',
             'rhgw = s3_extend.gateways.robohat_gateway:robohat_gateway',
